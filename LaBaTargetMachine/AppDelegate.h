e497d4270dcd346d3b97b0c934a17f13e435fcc7
－ios1 腾程 <tengcheng-ios1@tengchengzsw.local>
2016-10-17 09:17:20 +0800

    Initial Commit

---
+++ LaBaTargetMachine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LaBaTargetMachine
+//
+//  Created by 腾程－ios1 on 16/10/17.
+//  Copyright © 2016年 腾程－zsw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LaBaTargetMachine/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LaBaTargetMachine
+//
+//  Created by 腾程－ios1 on 16/10/17.
+//  Copyright © 2016年 腾程－zsw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
